e028d95951350ac7344e9cb23f17fd040fd35a95
Eric Rowell <erowell83@gmail.com>
2014-05-24 23:15:56 -0700

    added unix linux cheat sheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     staticHandlebars: {
       simpleTarget: {
-        files:{'dist/entertainment/*.html':'src/entertainment/*.hbs'}
+        files:{'dist/*/*.html':'src/*/*.hbs'}
       } 
     },
     watch: {
